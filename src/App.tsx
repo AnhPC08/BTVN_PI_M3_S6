34f7b321b0ba4eec63fbc7e41dfee5f8d8dff9cf
AnhPC <phamconghoananh2004bc@gmail.com>
2026-07-17 11:59:44 +0700

    cập nhật hiển thị BT9

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import WeatherWidget from "./BT5/WeatherWidget";
+import SmartCounter from "./BT9/SmartCounter";
 
 export default function App() {
   return (
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>Session 6 - Bài 5: Weather Widget</h1>
-
       <WeatherWidget />
+
+      <h3>Bài 9: Bộ đếm thông minh (TDD)</h3>
+      <SmartCounter />
     </div>
   );
 }
